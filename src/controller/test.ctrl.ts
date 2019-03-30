8f220fe361cb7ba371c44b592375da19c68b1cdd
jeffchoi <dev.jeffchoi@gmail.com>
2019-03-31 00:14:41 +0900

    보일러 플레이트 완성

---
+++ src/controller/test.ctrl.ts
@@ -0,0 +1,26 @@
+import { Context } from 'koa';
+import { getCustomRepository } from 'typeorm';
+import { TestRepo } from '../database/repository';
+
+const getText = async (ctx: Context) => {
+  const testRepo = getCustomRepository(TestRepo);
+
+  const test = testRepo.create();
+  test.name = Date.now().toLocaleString();
+
+  await testRepo.save(test);
+
+  const tests = await testRepo.find();
+  console.log('tests: ', tests);
+
+  ctx.status = 200;
+  ctx.body = {
+    code: 'SUCCESS',
+    message: '성공',
+    data: {
+      tests,
+    },
+  };
+};
+
+export { getText };
---
+++ src/database/index.ts
@@ -0,0 +1,37 @@
+import 'reflect-metadata';
+import { createConnection, ConnectionOptions } from 'typeorm';
+
+const {
+  DATABASE_HOST,
+  DATABASE_USER,
+  DATABASE_PASSWORD,
+  DATABASE_NAME,
+} = process.env;
+
+async function connectDatabase() {
+  try {
+    console.log(`Connecting database host: ${DATABASE_HOST}`);
+
+    const options: ConnectionOptions = {
+      type: 'mysql',
+      host: DATABASE_HOST,
+      port: 3306,
+      username: DATABASE_USER,
+      password: DATABASE_PASSWORD,
+      database: DATABASE_NAME,
+      entities: [__dirname + '/entity/*{.ts,.js}'],
+      timezone: 'Asia/Seoul',
+      extra: { max: 2, min: 1 },
+      synchronize: true,
+      logging: true,
+    };
+
+    await createConnection(options);
+
+    console.log(`Success to connect database host: ${DATABASE_HOST}`);
+  } catch (error) {
+    throw error;
+  }
+}
+
+export default connectDatabase;
